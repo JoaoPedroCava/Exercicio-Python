bf3dec23e748e1e05c58f04afb3f80253f558fec
João Pedro A. Cava <161586555+JoaoPedroCava@users.noreply.github.com>
2024-03-01 09:47:30 -0300

    Create DiasParaSegundos.py

---
+++ DiasParaSegundos.py
@@ -0,0 +1,6 @@
+d = int(input("Digite o numero de dias:"))
+h = int(input("Digite o numero de horas:"))
+m = int(input("Digite o numero de minutos:"))
+s = int(input("Digite o numero de segundos:"))
+
+print ("numero de segundos:", d*86400+h*3600+m*60+s)
